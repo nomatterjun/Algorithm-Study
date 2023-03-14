6ed59dd26be0bbd0529ed4134f46f453c11ee4a0
이창준 <nomatterjun@gmail.com>
2023-03-15 02:28:14 +0900

    vault backup: 2023-03-15 02:28:14

--- alg-materials-editions-4.0/06-linked-list/projects/starter/LinkedList.playground/Contents.swift
+++ alg-materials-editions-4.0/06-linked-list/projects/starter/LinkedList.playground/Contents.swift
@@ -1,3 +1,22 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+example(of: "creating and linking nodes") {
+  let node1 = Node(value: 1)
+  let node2 = Node(value: 2)
+  let node3 = Node(value: 3)
+  
+  node1.next = node2
+  node2.next = node3
+  
+  print(node1)
+}
+
+example(of: "push") {
+  var list = LinkedList<Int>()
+  list.push(3)
+  list.push(2)
+  list.push(1)
+
+  print(list)
+}
